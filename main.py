bddaa8bd3ed67d9f6b7963ffaf4fb0f771a10359
jonasrains <35318195+jonasrains@users.noreply.github.com>
2019-01-19 17:15:57 -0600

    made it so mario can move left/right

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 import pygame
-import mario
+import math
 
+import mario
 
 pygame.init()
 display = pygame.display.set_mode((512, 480))
 pygame.display.set_caption('Super Mario Odyssey Bros. 3')
 pygame.display.set_icon(pygame.image.load(r'./images/icon.png'))
 clock = pygame.time.Clock()
+pygame.font.init()
 
 running = True
 screen = 'title'
 world = 1
 level = 1
-
-
-def update_mario():
-    mario.update()
-
+right_pressed = False
+left_pressed = False
 
 mario.enter_level(world, level)
 
 while running:
-    update_mario()
+    mario.update(left_pressed, right_pressed)
     display.fill((182, 229, 235))
     for event in pygame.event.get():
         print(event)
@@ -30,6 +29,17 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 running = False
+
+            if event.key == pygame.K_RIGHT:
+                right_pressed = True
+            if event.key == pygame.K_LEFT:
+                left_pressed = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                right_pressed = False
+            if event.key == pygame.K_LEFT:
+                left_pressed = False
 
     if mario.direction == 90:
         display.blit(mario.image, (mario.xpos, mario.ypos))
--- mario.py
+++ mario.py
@@ -1,10 +1,14 @@
 import pygame
+import math
 
 direction = 90
 xpos = 0
 ypos = 0
 frame = 1
 running = False
+walk_anim = 1
+max_speed = 3
+velocity = 0
 image = pygame.image.load(r'./images/game elements/mario/' + str(frame) + '.png')
 
 
@@ -16,6 +20,50 @@ def enter_level(world, level):
         ypos = 0
 
 
-def update():
-    global image
+def update(left_pressed, right_pressed):
+    global image, frame, walk_anim, velocity, frame, direction, xpos, ypos
+
+    if not (right_pressed and left_pressed):
+        if right_pressed or left_pressed:
+            if walk_anim > 24:
+                walk_anim = 1
+            else:
+                walk_anim += 1
+        else:
+            if not 5 < walk_anim < 10:
+                if walk_anim > 24:
+                    walk_anim = 1
+                else:
+                    walk_anim += 1
+        if right_pressed:
+            direction = 90
+            if velocity < max_speed:
+                velocity += .1
+            else:
+                velocity = max_speed
+        elif left_pressed:
+            direction = -90
+            if velocity > - max_speed:
+                velocity -= .1
+            else:
+                velocity = - max_speed
+        else:
+            velocity = velocity * .8
+    else:
+        if not 5 < walk_anim < 10:
+            if walk_anim > 24:
+                walk_anim = 1
+            else:
+                walk_anim += 1
+        velocity = velocity * .8
+    xpos += velocity
+    if ((velocity < 0 and right_pressed) or (velocity > 0 and left_pressed)) and not(left_pressed and right_pressed):
+        frame = 6
+    else:
+        if math.ceil(walk_anim) / 6 == 1:
+            frame = 1
+        elif math.ceil(walk_anim) / 6 == 2 or math.ceil(walk_anim) / 6 == 4:
+            frame = 2
+        elif math.ceil(walk_anim) / 6 == 3:
+            frame = 3
     image = pygame.image.load(r'./images/game elements/mario/' + str(frame) + '.png')
