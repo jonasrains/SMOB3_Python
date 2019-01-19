15b823847b8f63f0167500bccc0d47776da07dc6
jonasrains <jonasrains@gmail.com>
2019-01-18 22:26:05 -0600

    initial commit

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ pygame.init()
 
 gamedisplay = pygame.display.set_mode((480, 360))
 pygame.display.set_caption('Super Mario Odyssey Bros. 3')
-
+a = pygame.image.load(r'./images/temp.png')
+pygame.display.set_icon(a)
 while True:
 
     for event in pygame.event.get():
