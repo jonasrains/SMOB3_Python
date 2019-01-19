670b27c6c01a8f65a4bffe92afb2871c070ba04f
jonasrains <35318195+jonasrains@users.noreply.github.com>
2019-01-19 13:10:00 -0600

    added ways to close

--- main.py
+++ main.py
@@ -1,14 +1,22 @@
 import pygame
 
 pygame.init()
-
 gamedisplay = pygame.display.set_mode((480, 360))
 pygame.display.set_caption('Super Mario Odyssey Bros. 3')
+pygame.display.set_icon(pygame.image.load(r'./images/icon.png'))
 
-while True:
+running = True
+
+while running:
 
     for event in pygame.event.get():
         print(event)
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                running = False
+
     pygame.display.update()
 
 pygame.quit()
