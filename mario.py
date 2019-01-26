5693bef9a290751476aaed9197687e6be0511a3b
jonasrains <35318195+jonasrains@users.noreply.github.com>
2019-01-25 20:17:23 -0600

    added jumping and semisolid platform collision

--- mario.py
+++ mario.py
@@ -20,6 +20,11 @@ max_speed = 3
 velocity = 0
 image = pygame.image.load(r'./images/game elements/mario/' + str(frame) + '.png')
 images = []
+jumping = False
+jump_rep = 0
+jump_height = 20
+up_released = False
+falling = False
 
 
 def load_images():
@@ -43,30 +48,75 @@ def touch_ground(world, level):
     return collision.collision([[xpos, xpos + 28], [ypos, ypos + 54]], data['ground'][str(world) + '-' + str(level)])
 
 
-def update(left_pressed, right_pressed, level, world):
-    global image, frame, walk_anim, velocity, frame, direction, xpos, ypos
-    while touch_ground(world, level)[0] and 2 not in touch_ground(world, level)[1]:
+def touch_semisolid(world, level):
+    for i in data['semisolid'][str(world) + '-' + str(level)]:
+        collide = collision.collision([[xpos, xpos + 28], [ypos + 52, ypos + 54]], i)
+        if collide[0]:
+            return collide
+    return [False, []]
+
+
+def touch_ceiling(world, level):
+    return collision.collision([[xpos, xpos + 28], [ypos, ypos + 2]], data['ground'][str(world) + '-' + str(level)])
+
+
+def jump(up_pressed, world, level):
+    global ypos, jumping, jump_rep, jump_height, up_released
+    ypos += 1
+    if (up_pressed and not jumping) and (touch_ground(world, level)[1] or touch_semisolid(world, level)[0]):
+        jumping = True
+        jump_rep = 0
+        jump_height = 20
+        up_released = False
+    ypos -= 1
+    if jumping and jump_rep < jump_height + 2:
+        jump_rep += 1
+        if not up_pressed and not up_released:
+            jump_height = jump_rep + 2
+            up_released = True
+        if jump_rep < jump_height:
+            for i in range(3 + round(jump_rep / 2)):
+                if not touch_ceiling(world, level)[0]:
+                    ypos -= 1
+        else:
+            for i in range(round(abs((jump_rep - jump_height)/2))):
+                if not touch_ceiling(world, level)[0]:
+                    ypos -= ((0-(abs(jump_rep - jump_height) / (jump_rep - jump_height)))*4)
+        if touch_ceiling(world, level)[0]:
+            ypos += 1
+            jumping = False
+    else:
+        jumping = False
+
+
+
+def update(left_pressed, right_pressed, up_pressed, level, world):
+    global image, frame, walk_anim, velocity, frame, direction, xpos, ypos, falling
+    falling = False
+    while (touch_ground(world, level)[0] and 2 not in touch_ground(world, level)[1]) or (touch_semisolid(world, level)[0] and 2 not in touch_semisolid(world, level)[1]) and not jumping:
         ypos -= 1
 
     ypos += 1
     if not (right_pressed and left_pressed):
         if right_pressed or left_pressed:
-            if touch_ground(world, level)[0]:
+            if touch_ground(world, level)[0] or touch_semisolid(world, level)[0]:
                 if walk_anim > 24:
                     walk_anim = 1
                 else:
                     walk_anim += 1
             else:
+                falling = True
                 walk_anim = 18
         else:
             if not math.ceil(walk_anim) / 6 == 1:
-                if touch_ground(world, level)[0]:
+                if touch_ground(world, level)[0] or touch_semisolid(world, level)[0]:
                     if walk_anim > 24:
                         walk_anim = 1
                     else:
                         walk_anim += 1
-                else:
-                    walk_anim = 18
+            if not (touch_ground(world, level)[0] or touch_semisolid(world, level)[0]):
+                falling = True
+                walk_anim = 18
         if right_pressed:
             direction = 90
             if velocity < max_speed:
@@ -83,13 +133,14 @@ def update(left_pressed, right_pressed, level, world):
             velocity = velocity * .8
     else:
         if not math.ceil(walk_anim) / 6 == 1:
-            if touch_ground(world, level)[0]:
+            if touch_ground(world, level)[0] or touch_semisolid(world, level)[0]:
                 if walk_anim > 24:
                     walk_anim = 1
                 else:
                     walk_anim += 1
-            else:
-                walk_anim = 18
+        if not (touch_ground(world, level)[0] or touch_semisolid(world, level)[0]):
+            falling = True
+            walk_anim = 18
         velocity = velocity * .8
         if abs(velocity) < .02:
             velocity = 0
@@ -103,11 +154,17 @@ def update(left_pressed, right_pressed, level, world):
         velocity = 0
     ypos += 1
 
-    for i in range(7):
-        if not touch_ground(world, level)[0]:
-            ypos += 1
+    jump(up_pressed, world, level)
 
-    if ((velocity < 0 and right_pressed) or (velocity > 0 and left_pressed)) and not(left_pressed and right_pressed):
+    if not jumping:
+        for i in range(6):
+            if not (touch_ground(world, level)[0] or touch_semisolid(world, level)[0]):
+                ypos += 1
+    if jumping:
+        frame = 4
+    elif falling:
+        frame = 3
+    elif ((velocity < 0 and right_pressed) or (velocity > 0 and left_pressed)) and not(left_pressed and right_pressed):
         frame = 6
     else:
         if math.ceil(walk_anim) / 6 == 1:
