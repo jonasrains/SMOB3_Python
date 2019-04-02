de8350de81d4a85e2466527196573ea571a35e50
jonasrains <35318195+jonasrains@users.noreply.github.com>
2019-04-02 15:18:31 -0500

    added some preparations for cappy, added crouching, added offsety

--- mario.py
+++ mario.py
@@ -1,30 +1,31 @@
 import pygame
 import math
-
 import collision
 import json
+import cappy
 
 json_data = open('hitboxes.json').read()
-
-data = json.loads(json_data)
-print(data)
+hitboxes = json.loads(json_data)
+print(hitboxes)
 
 direction = 90
 ground_hitbox = []
 xpos = 0
 ypos = 0
+yoffset = 0
 frame = 1
 running = False
 walk_anim = 1
 max_speed = 3
 velocity = 0
-image = pygame.image.load(r'./images/game elements/mario/' + str(frame) + '.png')
+image = pygame.image.load(r'./images/game elements/mario/1.png')
 images = []
 jumping = False
 jump_rep = 0
 jump_height = 20
 up_released = False
 falling = False
+crouching = False
 
 
 def load_images():
@@ -36,52 +37,59 @@ def load_images():
 def enter_level(world, level):
     global running, xpos, ypos, ground_hitbox
     load_images()
+    cappy.load_images()
     running = True
-    ground_hitbox = data['ground'][str(world) + '-' + str(level)]
+    ground_hitbox = hitboxes['ground'][str(world) + '-' + str(level)]
     print(ground_hitbox)
     if world == 1 and level == 1:
-        xpos = 0
+        xpos = 16
         ypos = 0  # 280
 
 
 def touch_ground(world, level):
-    return collision.collision([[xpos, xpos + 28], [ypos, ypos + 54]], data['ground'][str(world) + '-' + str(level)])
+    return collision.collision([[xpos - 16, xpos + 16], [ypos - 32, ypos + 22]], hitboxes['ground'][str(world) + '-' + str(level)])
 
 
 def touch_semisolid(world, level):
-    for i in data['semisolid'][str(world) + '-' + str(level)]:
-        collide = collision.collision([[xpos, xpos + 28], [ypos + 52, ypos + 54]], i)
+    for i in hitboxes['semisolid'][str(world) + '-' + str(level)]:
+        collide = collision.collision([[xpos - 16, xpos + 16], [ypos + 20, ypos + 22]], i)
         if collide[0]:
             return collide
     return [False, []]
 
 
 def touch_ceiling(world, level):
-    return collision.collision([[xpos, xpos + 28], [ypos, ypos + 2]], data['ground'][str(world) + '-' + str(level)])
+    return collision.collision([[xpos - 16, xpos + 16], [ypos - 32, ypos - 30]], hitboxes['ground'][str(world) + '-' + str(level)])
 
+
+def crouch(down_pressed):
+    global crouching
+    if down_pressed:
+        crouching = True
+    else:
+        crouching = False
 
 def jump(up_pressed, world, level):
     global ypos, jumping, jump_rep, jump_height, up_released
     ypos += 1
+    jump_rep += 1
     if (up_pressed and not jumping) and (touch_ground(world, level)[1] or touch_semisolid(world, level)[0]):
         jumping = True
         jump_rep = 0
         jump_height = 20
         up_released = False
     ypos -= 1
-    if jumping and jump_rep < jump_height + 2:
-        jump_rep += 1
+    if jumping and jump_rep < jump_height + 3:
         if not up_pressed and not up_released:
-            jump_height = jump_rep + 2
+            jump_height = jump_rep
             up_released = True
         if jump_rep < jump_height:
             for i in range(3 + round(jump_rep / 2)):
                 if not touch_ceiling(world, level)[0]:
                     ypos -= 1
         else:
-            for i in range(round(abs((jump_rep - jump_height)/2))):
-                if not touch_ceiling(world, level)[0]:
-                    ypos -= ((0-(abs(jump_rep - jump_height) / (jump_rep - jump_height)))*4)
+            if not touch_ceiling(world, level)[0]:
+                ypos -= (jump_rep - jump_height - 1)
         if touch_ceiling(world, level)[0]:
             ypos += 1
             jumping = False
@@ -89,15 +97,15 @@ def jump(up_pressed, world, level):
         jumping = False
 
 
-
-def update(left_pressed, right_pressed, up_pressed, level, world):
-    global image, frame, walk_anim, velocity, frame, direction, xpos, ypos, falling
+def update(left_pressed, right_pressed, up_pressed, down_pressed, level, world):
+    global image, frame, walk_anim, velocity, frame, direction, xpos, ypos, falling, yoffset
     falling = False
+    crouch(down_pressed)
     while (touch_ground(world, level)[0] and 2 not in touch_ground(world, level)[1]) or (touch_semisolid(world, level)[0] and 2 not in touch_semisolid(world, level)[1]) and not jumping:
         ypos -= 1
 
     ypos += 1
-    if not (right_pressed and left_pressed):
+    if not (right_pressed and left_pressed) and (not crouching or (crouching and not (touch_ground(world, level)[1] or touch_semisolid(world, level)[0]))):
         if right_pressed or left_pressed:
             if touch_ground(world, level)[0] or touch_semisolid(world, level)[0]:
                 if walk_anim > 24:
@@ -154,23 +162,30 @@ def update(left_pressed, right_pressed, up_pressed, level, world):
         velocity = 0
     ypos += 1
 
+
     jump(up_pressed, world, level)
 
     if not jumping:
         for i in range(6):
             if not (touch_ground(world, level)[0] or touch_semisolid(world, level)[0]):
                 ypos += 1
-    if jumping:
-        frame = 4
-    elif falling:
-        frame = 3
-    elif ((velocity < 0 and right_pressed) or (velocity > 0 and left_pressed)) and not(left_pressed and right_pressed):
-        frame = 6
+
+    yoffset = 0
+    if crouching:
+        yoffset = 18
+        frame = 5
     else:
-        if math.ceil(walk_anim) / 6 == 1:
-            frame = 1
-        elif math.ceil(walk_anim) / 6 == 2 or math.ceil(walk_anim) / 6 == 4:
-            frame = 2
-        elif math.ceil(walk_anim) / 6 == 3:
+        if jumping:
+            frame = 4
+        elif falling:
             frame = 3
+        elif ((velocity < 0 and right_pressed) or (velocity > 0 and left_pressed)) and not(left_pressed and right_pressed):
+            frame = 6
+        else:
+            if math.ceil(walk_anim) / 6 == 1:
+                frame = 1
+            elif math.ceil(walk_anim) / 6 == 2 or math.ceil(walk_anim) / 6 == 4:
+                frame = 2
+            elif math.ceil(walk_anim) / 6 == 3:
+                frame = 3
     image = images[frame - 1]
